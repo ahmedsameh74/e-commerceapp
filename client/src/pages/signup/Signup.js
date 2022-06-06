f55d1fb05a811a030aaa9ad7913c59d7f5bd3ea3
Farah Elsayed <faraahameeen@gmail.com>
2022-06-07 01:52:15 +0200

    sign up

--- client/src/pages/signup/Signup.js
+++ client/src/pages/signup/Signup.js
@@ -5,47 +5,52 @@ import "./Signup.css";
 const isEmailValid = (value) =>
   value.trim() !== "" && value.includes("@" || ".com");
 const isDataValid = (value) => value.trim() !== "";
-const isPasswordValid = (value) => value.trim() !== "" && value.length > 8;
 
 export default function Signup() {
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
-  const [password, setPassword] = useState("");
-  const [shippingAddress, setShippingAddress] = useState("");
   const [error, setError] = useState(false);
-
-  let formIsValid = false;
-  if (
-    isEmailValid(email) &&
-    isPasswordValid(password) &&
-    isDataValid(phone) &&
-    isDataValid(name) &&
-    isDataValid(shippingAddress)
-  ) {
-    formIsValid = true;
-  }
-
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    phone: "",
+    shippingAddress: "",
+    name: "",
+  });
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!formIsValid) {
-      setError(true)
+    if (
+      !isEmailValid(user.email) ||
+      !isDataValid(user.password) ||
+      !isDataValid(user.phone) ||
+      !isDataValid(user.name) ||
+      !isDataValid(user.shippingAddress)
+    ) {
+      setError(true);
       return;
     } else {
+      console.log(user);
       setError(false);
-      console.log(email, password, name);
-      setEmail("");
-      setPassword("");
-      setName("");
-      setPhone("");
-      setShippingAddress("");
+      fetch("http://ecommerceapp0040.herokuapp.com/api/register", {
+        method: "POST",
+        body: JSON.stringify({
+          name: user.name,
+          password: user.password,
+          email: user.email,
+          phone: user.phone,
+          shippingAddress: user.shippingAddress,
+        }),
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((res) => res.json())
+        .then((data) => {});
+      setUser({
+        name: "",
+        email: "",
+        password: "",
+        shippingAddress: "",
+        phone: "",
+      });
     }
   };
-  //   fetch("https://ecommerce-app0040.herokuapp.com/api/register", {
-  //       method: 'POST',
-  //       body:
-  //   })
-
   return (
     <div className="signup">
       <form onSubmit={handleSubmit}>
@@ -56,10 +61,13 @@ export default function Signup() {
           <input
             type="text"
             name=""
+            style={{ border: error ? '1px solid red ': "" }}
             id="name"
-            onChange={(e) => setName(e.target.value)}
-            value={name}
-            className={error && "error"}
+            onChange={(event) => {
+              setUser({ ...user, name: event.target.value });
+            
+            }}
+            value={user.name}
             placeholder="ex: Jane Doe"
           />
         </div>
@@ -69,9 +77,12 @@ export default function Signup() {
             type="email"
             name=""
             id="email"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            className={error && "error"}
+            onChange={(event) => {
+              setUser({ ...user, email: event.target.value });
+           
+            }}
+            value={user.email}
+            style={{ border: error ? '1px solid red ': "" }}
             placeholder="example@gmail.com"
           />
         </div>
@@ -81,9 +92,12 @@ export default function Signup() {
             type="password"
             name=""
             id="password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            className={error && "error"}
+            onChange={(event) => {
+              setUser({ ...user, password: event.target.value });
+              
+            }}
+            value={user.password}
+            style={{ border: error ? '1px solid red ': "" }}
             placeholder="********"
           />
         </div>
@@ -93,9 +107,12 @@ export default function Signup() {
             type="tel"
             name=""
             id="phone"
-            onChange={(e) => setPhone(e.target.value)}
-            value={phone}
-            className={error && "error"}
+            onChange={(event) => {
+              setUser({ ...user, phone: event.target.value });
+              
+            }}
+            value={user.phone}
+            style={{ border: error ? '1px solid red ': "" }}
             placeholder="ex: 012345678"
           />
         </div>
@@ -106,9 +123,12 @@ export default function Signup() {
             type="text"
             name=""
             id="shippingAddress"
-            onChange={(e) => setShippingAddress(e.target.value)}
-            value={shippingAddress}
-            className={error && "error"}
+            onChange={(event) => {
+              setUser({ ...user, shippingAddress: event.target.value });
+             
+            }}
+            value={user.shippingAddress}
+            style={{ border: error ? '1px solid red ': "" }}
             placeholder="Apt, floor no"
           />
         </div>
@@ -116,7 +136,7 @@ export default function Signup() {
         <span className="registered">
           Already have an account? <Link to="/login">Log In</Link>
         </span>
-        <button disabled={!formIsValid}>Submit</button>
+        <button>Submit</button>
       </form>
     </div>
   );
